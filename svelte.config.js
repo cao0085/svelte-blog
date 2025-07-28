ee2f079463e2842cfc615108b80c86cc1a575fe3
tony <tonyko@youparking.com.tw>
2025-07-28 13:38:06 +0800

    test action

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import path from 'path';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	extensions: ['.svelte', '.md'],
+// 	preprocess: [
+// 		vitePreprocess(),
+// 		mdsvex({ extensions: ['.md'] })
+// 	],
+// 	kit: {
+// 		adapter: adapter(),
+// 		alias: {
+// 		$lib: path.resolve('./src/lib'),
+// 		$content: path.resolve('./src/content')
+// 		}
+// 	}
+// };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,10 +28,17 @@ const config = {
 		mdsvex({ extensions: ['.md'] })
 	],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		alias: {
-		$lib: path.resolve('./src/lib'),
-		$content: path.resolve('./src/content')
+			$lib: path.resolve('./src/lib'),
+			$content: path.resolve('./src/content')
+		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/svelte-blog' : ''
 		}
 	}
 };
